d51028efd638d2c2e679b423e4dbea6113cc08a8
Alan Xu <alan.xu1699@gmail.com>
2023-02-25 15:20:32 -0500

    style: Customized headings section of landing page

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -13,14 +13,42 @@ const Landing = ({ setSelectedPage }) => {
             {/* IMAGE */}
             <div className="md:order-2 flex justify-center basis-3/5 mt-16 md:mt-32">
                 {isAboveMediumScreens ? (
-                    <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:w-full before:max-w-[500px] before:h-full before:border-2 before:border-blue before:z-[-1]">
-                        <img src={profile} className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[500px]" alt="profile" />
+                    <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:w-full before:max-w-400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
+                        <img src={profile} className="hover:filter hover:saturate-100 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[300px]" alt="profile" />
                     </div>
                 ) : (
-                    <img src={profile} className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[500px]" alt="profile" />
+                    <img src={profile} className="hover:filter hover:saturate-100 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[300px]" alt="profile" />
                 )}
+            </div>
 
-                {/* MAIN SECTION */}
+            {/* MAIN SECTION */}
+            <div className="z-30 basis-2/5 mt-12 md:mt-32">
+                {/* HEADINGS */}
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
+                >
+                    <p className="text-6xl font-playfair z-10 text-center md:text-start">
+                        Alan {""}
+                        <span
+                            className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush  before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"
+                        >
+                            Xu
+                        </span>
+                    </p>
+
+                    <p className="mt-10 mb-7 text-sm text-center md:text-start">
+                        Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum
+                        viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
+                    </p>
+                </motion.div>
+
             </div>
         </section>
     )
