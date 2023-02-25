1d51f354830746a58ef5a0d6215e11f5df697436
Alan Xu <alan.xu1699@gmail.com>
2023-02-24 19:53:35 -0500

    feat: Finished navbar feature Navbar should be able be direct users to the desired section with the use of anchorlinks from anchor-link package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
@@ -11,13 +12,29 @@ function App() {
   //check if min-width is less than/greater than 1060px
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)"); 
 
-  useEFF
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app bg-deep-blue">
       <Navbar
         isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}
       />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+      </div>
     </div>
   );
 }
