f2974fe250ce2b4a0f56cbe29732db1fa6abc107
Alan Xu <alan.xu1699@gmail.com>
2023-02-25 14:45:40 -0500

    style: Customized landing page with profile picture Changed profile picture on the landing page to have a background border color of blue. Upon hover the profile picture has a filter with a saturation of 150 and when above medium screen sizes the picture will have a max width of 500px. For mobile versions, the background border will disappear and show only the picture

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -2,7 +2,7 @@ import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
-import profile from "../assets/profile-image.png";
+import profile from "../assets/my-profile-pic.jpg";
 
 const Landing = ({ setSelectedPage }) => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
@@ -13,12 +13,12 @@ const Landing = ({ setSelectedPage }) => {
             {/* IMAGE */}
             <div className="md:order-2 flex justify-center basis-3/5 mt-16 md:mt-32">
                 {isAboveMediumScreens ? (
-                    <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
-                        <img src={profile} className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]" alt="profile" />
+                    <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:w-full before:max-w-[500px] before:h-full before:border-2 before:border-blue before:z-[-1]">
+                        <img src={profile} className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[500px]" alt="profile" />
                     </div>
                 ) : (
                     <div>
-                        <img src={profile} className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]" alt="profile" />
+                        <img src={profile} className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[500px]" alt="profile" />
                     </div>
                 )}
 
