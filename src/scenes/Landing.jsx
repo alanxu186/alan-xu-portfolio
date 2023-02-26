b92fd3dc2a6c658cb005bad8389b4c6b3c7c3a7a
Alan Xu <alan.xu1699@gmail.com>
2023-02-25 22:18:32 -0500

    chore: Create SocialMediaIcons.jsx file and imported it in Landing.jsx

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -4,6 +4,8 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 
 import profile from "../assets/my-profile-pic.jpg";
 
+import SocialMediaIcons from "../scenes/SocialMediaIcons";
+
 const Landing = ({ setSelectedPage }) => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
@@ -76,6 +78,20 @@ const Landing = ({ setSelectedPage }) => {
                             Let's talk
                         </div>
                     </AnchorLink>
+                </motion.div>
+
+                <motion.div
+                    className="flex mt-5 justify-center md:justify-start"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.4, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
+                >
+                    <SocialMediaIcons/>
                 </motion.div>
             </div>
         </section>
