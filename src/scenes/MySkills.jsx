c63ef2bb9836cf3920b43818ad87bafe459624ba
Alan Xu <alan.xu1699@gmail.com>
2023-02-26 13:20:35 -0500

    feat: Complete MySkills.jsx file with framer-motion desc: The MySkills.jsx file serves as a temporary template that I will change after finishing the rest of the portfolio. It will be replaced with a frontend and backend skills section

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -38,9 +38,88 @@ const MySkills = () => {
                             <img src={skill} className="z-10" alt="skill" />
                         </div>
                     ) : (
-                        <img src={skill} alt="skill"/>
+                        <img src={skill} alt="skill" />
                     )}
                 </div>
+            </div>
+
+
+            {/* SKILLS */}
+            <div className="md:flex md:justify-between mt-16 gap-32">
+                {/* EXPERIENCE */}
+                <motion.div
+                    className="md:w-1/3 mt-10"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                    }}>
+                    <div className="relative h-32">
+                        <div className="z-10">
+                            <p className="font-playfair font-semibold text-5xl"> 01</p>
+                            <p className="font-playfair font-semibold text-3xl mt-3">
+                                Experience
+                            </p>
+                        </div>
+                        <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]"></div>
+                    </div>
+                    <p className="mt-5">
+                        Aliquam, some random line of temporary text because this section is still in works and will contain my featured set of skills that I have yet to put.
+                    </p>
+                </motion.div>
+
+                {/* INNOVATIVE */}
+                <motion.div
+                    className="md:w-1/3 mt-10"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                    }}>
+                    <div className="relative h-32">
+                        <div className="z-10">
+                            <p className="font-playfair font-semibold text-5xl">02</p>
+                            <p className="font-playfair font-semibold text-3xl mt-3">
+                                Innovative
+                            </p>
+                        </div>
+                        <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]"></div>
+                    </div>
+                    <p className="mt-5">
+                        Segundo, some random line of temporary text because this section is still in works and will contain my featured set of skills that I have yet to put.
+                    </p>
+                </motion.div>
+
+                {/* IMAGINATIVE */}
+                <motion.div
+                    className="md:w-1/3 mt-10"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.4, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                    }}>
+                    <div className="relative h-32">
+                        <div className="z-10">
+                            <p className="font-playfair font-semibold text-5xl">03</p>
+                            <p className="font-playfair font-semibold text-3xl mt-3">
+                                Imaginative
+                            </p>
+                        </div>
+                        <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]"></div>
+                    </div>
+                    <p className="mt-5">
+                        Thirderino, some random line of temporary text because this section is still in works and will contain my featured set of skills that I have yet to put.
+                    </p>
+                </motion.div>
             </div>
         </section>
     )
